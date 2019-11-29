32f7ebba473df3503fef96fa45ba02e791aeee01
mehmetyavuz <emirmehmetyavuz@gmail.com>
2019-11-29 20:46:41 +0300

    Initial Commit

---
+++ Lugat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lugat
+//
+//  Created by mehmet on 29.11.2019.
+//  Copyright © 2019 misbah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
